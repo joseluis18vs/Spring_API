d41ed8fcb2a91da92454a6ef7d7eb49bca017e1b
Jose Vanegas <joseluis18vs@gmail.com>
2022-10-31 00:44:42 +0100

    Reset

--- src/main/java/com/api_rest/e_springboot_example_jpa_apirest/Repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.api_rest.e_springboot_example_jpa_apirest.Repository;
-
-import com.api_rest.e_springboot_example_jpa_apirest.Entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {}
